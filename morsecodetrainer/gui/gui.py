f14a5251154caf7a28cfa03ed99752ca4c17fb22
ntouran <partofthething@users.noreply.github.com>
2015-05-11 22:27:03 -0700

    Added justlast input to allow you to focus on most recently learned chars
    Now remembers how many letters you're up to
    Did some structural refactoring in plotting

--- morsecodetrainer/gui/gui.py
+++ morsecodetrainer/gui/gui.py
@@ -21,21 +21,25 @@ EVT_DONE = wx.PyEventBinder(myEVT_DONE, 1)
 
 class MyFrame(wx.Frame):
     def __init__(self, *args, **kwds):
-        #kwds["style"] = wx.DEFAULT_FRAME_STYLE
+        # kwds["style"] = wx.DEFAULT_FRAME_STYLE
         wx.Frame.__init__(self, *args, **kwds)
         # begin wxGlade: MyFrame.__init__
         self.num_words = 0
         self.answers = []
         self.morse_sound = None
-        
+        self.trainer = GuiTrainer(self)
+
         self.panel = wx.Panel(self, wx.ID_ANY)
-        self.level = wx.SpinCtrl(self.panel, wx.ID_ANY, "2", min=2, max=35)
+        self.level = wx.SpinCtrl(self.panel, wx.ID_ANY, "{0}".format(self.trainer.get_historical_max_chars()),
+                                 min=2, max=35)
+        self.justlast = wx.SpinCtrl(self.panel, wx.ID_ANY, "0", min=0, max=35)
         self.level_label = wx.StaticText(self.panel, wx.ID_ANY, "Number of letters", style=wx.ALIGN_RIGHT)
+        self.justlast_label = wx.StaticText(self.panel, wx.ID_ANY, "Just x last letters", style=wx.ALIGN_RIGHT)
         self.accuracy = wx.StaticText(self.panel, wx.ID_ANY, "Accuracy: 0%")
         self.start = wx.Button(self.panel, wx.ID_ANY, "Start Training Session")
         self.stop = wx.Button(self.panel, wx.ID_ANY, "Stop Training Session")
         self.morsegrid = wx.grid.Grid(self.panel, wx.ID_ANY, size=(1, 1))
-        
+
         # Menu Bar
         self.menu = wx.MenuBar()
         self.file = wx.Menu()
@@ -62,12 +66,12 @@ class MyFrame(wx.Frame):
         self.__set_properties()
         self.__do_layout()
         # end wxGlade
-        
+
         self.timer = wx.Timer(self, wx.ID_ANY)
         self.Bind(wx.EVT_TIMER, self.on_timer)
-        
-        self.trainer = GuiTrainer(self)
-        self.timer.Start(1000)    # 1 second interval
+
+
+        self.timer.Start(1000)  # 1 second interval
 
     def on_plot(self, event):
         """
@@ -107,10 +111,12 @@ class MyFrame(wx.Frame):
         lower_control_sizer = wx.BoxSizer(wx.HORIZONTAL)
         lower_control_sizer.Add(self.start, 0, 0, 0)
         lower_control_sizer.Add(self.stop, 0, 0, 0)
-        upper_control_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        upper_control_sizer = wx.GridSizer(2, 2, 2, 2)
         upper_control_sizer.Add(self.level, 0, 0, 0)
         upper_control_sizer.Add(self.level_label, 0, 0, 0)
-        control_sizer_base.Add(upper_control_sizer, 1,  wx.ALIGN_CENTER_HORIZONTAL, 0)
+        upper_control_sizer.Add(self.justlast, 0, 0, 0)
+        upper_control_sizer.Add(self.justlast_label, 0, 0, 0)
+        control_sizer_base.Add(upper_control_sizer, 1, wx.ALIGN_CENTER_HORIZONTAL, 0)
         control_sizer_base.Add(self.accuracy, 0, wx.ALIGN_CENTER_HORIZONTAL, 0)
         control_sizer_base.Add(lower_control_sizer, 0, wx.ALIGN_CENTER_HORIZONTAL, 0)
         two_main_columns.Add(control_sizer_base, 1, wx.ALIGN_CENTER_VERTICAL, 0)
@@ -118,44 +124,44 @@ class MyFrame(wx.Frame):
         self.panel.SetSizer(two_main_columns)
         main_sizer.Add(self.panel, 1, wx.EXPAND, 0)
         self.SetSizer(main_sizer)
-        #main_sizer.Fit(self)
+        # main_sizer.Fit(self)
         self.Layout()
         # end wxGlade
 
     def on_start(self, event):
         """
         Start the training.
-        
-        Start it in another thread. 
+
+        Start it in another thread.
         """
         self.num_words = 0
         self.answers = []
         self.morsegrid.ClearGrid()
         for row in range(int(config.MINUTES_OF_TRAINING * WORDS_PER_MINUTE) + 100):
             self.morsegrid.SetCellBackgroundColour(row, 2, wx.WHITE)
-        
+
         self.morse_sound = SoundThread(self)
         self.morse_sound.start()
         self.morsegrid.SetCellBackgroundColour(0, 0, wx.GREEN)
         self.morsegrid.ForceRefresh()
-        
+
     def on_stop(self, event):
         """
         Stop the training.
-. 
+.
         """
         self.morse_sound.stop()
-        
-        
+
+
     def on_new_word(self, evt):
         new_word = evt.GetValue()
         self.morsegrid.SetCellBackgroundColour(self.num_words, 0, wx.WHITE)
         self.morsegrid.SetCellBackgroundColour(self.num_words + 1, 0, wx.GREEN)
         self.morsegrid.ForceRefresh()
         #
-        self.num_words +=1
+        self.num_words += 1
         self.answers.append(new_word)
-        
+
     def on_done(self, evt):
         """
         Training is done. Show results
@@ -163,22 +169,22 @@ class MyFrame(wx.Frame):
         correct = 0
         ai = 0
         self.morsegrid.SetCellBackgroundColour(self.num_words, 0, wx.WHITE)
-        for ai,answer in enumerate(self.answers):
+        for ai, answer in enumerate(self.answers):
             self.morsegrid.SetCellValue(ai, 1, answer)
             user_input = self.morsegrid.GetCellValue(ai, 0)
             if user_input.upper() == answer:
                 correct += 1
                 self.morsegrid.SetCellBackgroundColour(ai, 2, wx.GREEN)
                 self.morsegrid.SetCellValue(ai, 2, 'Yes!')
-                
+
             else:
                 self.morsegrid.SetCellValue(ai, 2, 'No')
-                
-        self.accuracy_value =  correct / float(ai+1) * 100
+
+        self.accuracy_value = correct / float(ai + 1) * 100
         self.accuracy.SetLabel('Accuracy: {0:3.0f}%'.format(self.accuracy_value))
         if self.trainer.full_run_completed:
             self.trainer.log_results(self.accuracy_value)
-            
+
 
 class SoundThread(threading.Thread):
     """
@@ -190,24 +196,25 @@ class SoundThread(threading.Thread):
 
     def run(self):
         self._parent.trainer.set_num_characters(self._parent.level.GetValue())
+        self._parent.trainer.set_justlast_characters(self._parent.justlast.GetValue())
         self._parent.trainer.run()
-        time.sleep(2) # give user enough time to finish last entry. 
+        time.sleep(2)  # give user enough time to finish last entry.
         self.end_training()
-        
+
     def stop(self):
         self._parent.trainer.stop()
         self.end_training()
-        
+
     def end_training(self):
         evt = DoneEvent(myEVT_DONE, -1)
         wx.PostEvent(self._parent, evt)
-        
+
 
 class GuiTrainer(Trainer):
     def __init__(self, parent):
         self._parent = parent
         Trainer.__init__(self)
-        
+
     def render_correct_answer(self, word):
         evt = NewWordEvent(myEVT_NEWWORD, -1, word)
         wx.PostEvent(self._parent, evt)
@@ -217,28 +224,27 @@ class NewWordEvent(wx.PyCommandEvent):
         """Creates the event object"""
         wx.PyCommandEvent.__init__(self, etype, eid)
         self._value = value
-        
+
     def GetValue(self):
         return self._value
-    
+
 class DoneEvent(wx.PyCommandEvent):
     """
-    Training is done. 
+    Training is done.
     """
     def __init__(self, etype, eid, value=None):
         """Creates the event object"""
         wx.PyCommandEvent.__init__(self, etype, eid)
         self._value = value
-        
+
     def GetValue(self):
         return self._value
-    
+
 if __name__ == "__main__":
-    gettext.install("app") # replace with the appropriate catalog name
+    gettext.install("app")  # replace with the appropriate catalog name
     app = wx.PySimpleApp(0)
     wx.InitAllImageHandlers()
-    gui = MyFrame(None, title = "Morse Code",size=(700, 600))
+    gui = MyFrame(None, title="Morse Code", size=(700, 600))
     app.SetTopWindow(gui)
     gui.Show()
     app.MainLoop()
-    
--- morsecodetrainer/trainer.py
+++ morsecodetrainer/trainer.py
@@ -11,25 +11,29 @@ from datetime import datetime
 from morsecodelib import sound
 import config
 
-KOCH_LETTER_ORDER=['K', 'M', 'R', 'S','U','A','P','T','L','O','W','I','.','N','J','E','F','0','Y',
-                   'V', 'G', '5', '/','Q','9','Z','H','3','8','B','?','4','2','7','C','1','D','6','X']
+KOCH_LETTER_ORDER = ['K', 'M', 'R', 'S', 'U', 'A', 'P', 'T', 'L', 'O', 'W', 'I', '.', 'N', 'J', 'E', 'F', '0', 'Y',
+                   'V', 'G', '5', '/', 'Q', '9', 'Z', 'H', '3', '8', 'B', '?', '4', '2', '7', 'C', '1', 'D', '6', 'X']
 
 class Trainer(object):
     def __init__(self):
         self.stopped = False
         self.elapsed_seconds = 0.0
         self.num_characters = 2
-        self.full_run_completed = False # for determining whether or not to log score
-  
+        self.justlast = 0
+        self.full_run_completed = False  # for determining whether or not to log score
+
     def run(self):
         start = time.time()
         self.stopped = False
         morse_sound = sound.MorseSoundPlayer()
         self.elapsed_seconds = 0.0
         self.full_run_completed = False
-        while self.elapsed_seconds < config.MINUTES_OF_TRAINING*60.0:
+        valid_letters = KOCH_LETTER_ORDER[:self.num_characters]
+        if self.justlast:
+            valid_letters = valid_letters[-self.justlast:]
+        while self.elapsed_seconds < config.MINUTES_OF_TRAINING * 60.0:
             length = self.get_length_of_phrase()
-            letters = [random.choice(KOCH_LETTER_ORDER[:self.num_characters]) for _i in range(length)]
+            letters = [random.choice(valid_letters) for _i in range(length)]
             word = ''.join(letters)
             morse_sound.text_to_sound(word)
             self.elapsed_seconds = time.time() - start
@@ -37,88 +41,116 @@ class Trainer(object):
             if self.stopped:
                 break
         else:
-            self.full_run_completed = True
-            
+            if not self.justlast:
+                # don't log situations when user is just focusing on most recent letters.
+                self.full_run_completed = True
+
+        morse_sound.stop()
+
+
+
     def get_length_of_phrase(self):
         """
-        Randomly determine number of chars to use. 
-        
-        Uniform distributions seem to put too many at 1 or 2 characters. 
+        Randomly determine number of chars to use.
+
+        Uniform distributions seem to put too many at 1 or 2 characters.
         """
-        
-        #random.choice(range(1,7))
-        num = int(random.gauss(4,1.5))
-        if num>7:
+
+        # random.choice(range(1,7))
+        num = int(random.gauss(4, 1.5))
+        if num > 7:
             num = 7
-        elif num<1:
+        elif num < 1:
             num = 1
         return num
-    
+
     def render_correct_answer(self, word):
         """print out latest correct answer"""
         print word
-    
+
     def set_num_characters(self, num_characters):
         self.num_characters = num_characters
-        
+
+    def set_justlast_characters(self, num_off_end):
+        """
+        Allows user to focus on only the most recently added few characters.
+        """
+        self.justlast = num_off_end
+
     def stop(self):
         self.stopped = True
-        
+
     def log_results(self, accuracy_value):
         """
         Store results so you can see your history
         """
-        with open(self.get_log_file_name(),'a') as log_file:
-            log_file.write('<{0}> {1} {2} {3}\n'.format(time.ctime(), self.num_characters, 
+        with open(self.get_log_file_name(), 'a') as log_file:
+            log_file.write('<{0}> {1} {2} {3}\n'.format(time.ctime(), self.num_characters,
                                                         config.MINUTES_OF_TRAINING, accuracy_value))
 
     def get_log_file_name(self):
-        
-        log_directory = os.path.join(os.path.expanduser('~'),'.morescodetrainer')
+
+        log_directory = os.path.join(os.path.expanduser('~'), '.morescodetrainer')
         if not os.path.exists(log_directory):
             os.makedirs(log_directory)
-        return os.path.join(log_directory,'accuracy_history.txt')
-    
+        return os.path.join(log_directory, 'accuracy_history.txt')
+
     def plot_history(self):
         """
         Plot training progress as logged
         """
         from matplotlib import pyplot as plt
-        
-        log_file_name = self.get_log_file_name()
-        if not os.path.exists(log_file_name):
-            print('No log file to plot')
-            return
-    
-        data = []
-        with open(log_file_name) as log_file:
-            for line in log_file:
-                match = re.search('<(.+?)> (\d+) (\d+) (\S+)',line)
-                if not match:
-                    print('Corrupted log entry: {0}'.format(line))
-                timestamp = datetime.fromtimestamp(time.mktime(time.strptime(match.group(1))))
-                num_chars = int(match.group(2))
-                duration = float(match.group(3))
-                accuracy = float(match.group(4)) 
-                data.append((timestamp, num_chars, duration, accuracy))
-                
-        times, chars, durations, accuracy = zip(*data)
-        
+
+
+        times, chars, durations, accuracy = self.read_log()
+
         transitions = self.get_new_letter_dates(times, chars)
-        
+
         fig, ax = plt.subplots()
         ax.plot_date(times, accuracy, '-o')
         fig.autofmt_xdate()
         plt.title('Morse Code learning progress')
         plt.ylabel('Accuracy (%)')
-        
+
         for transition in transitions:
-            plt.axvline(x = transition,c='BLACK')
-            
+            plt.axvline(x=transition, c='BLACK')
+
         plt.axhline(y=90, c='RED')
-        
+
         plt.show()
-        
+
+    def read_log(self):
+
+        log_file_name = self.get_log_file_name()
+        if not os.path.exists(log_file_name):
+            print('No log file to plot')
+            return
+
+        data = []
+        with open(log_file_name) as log_file:
+            for line in log_file:
+                match = re.search('<(.+?)> (\d+) (\d+) (\S+)', line)
+                if not match:
+                    print('Corrupted log entry: {0}'.format(line))
+                timestamp = datetime.fromtimestamp(time.mktime(time.strptime(match.group(1))))
+                num_chars = int(match.group(2))
+                duration = float(match.group(3))
+                accuracy = float(match.group(4))
+                data.append((timestamp, num_chars, duration, accuracy))
+
+        times, chars, durations, accuracy = zip(*data)
+        return times, chars, durations, accuracy
+
+    def get_historical_max_chars(self):
+        """
+        return the highgest number of characters the user has logged so far.
+        """
+        try:
+            _times, chars, _durations, _accuracy = self.read_log()
+        except:
+            return 2
+        return max(chars)
+
     def get_new_letter_dates(self, times, chars):
         """
         Find dates where we added a new letter
@@ -132,8 +164,8 @@ class Trainer(object):
                 max_num_chars = num_chars
             last_tm = tm
         return transitions
-        
-        
+
+
 if __name__ == '__main__':
     trainer = Trainer()
     trainer.plot_history()
